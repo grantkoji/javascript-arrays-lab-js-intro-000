db808dbd17794a88b3bdc8da73c3be4812f02d25
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-05 20:06:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 // Add your functions and code here
-function destructivelyAppendKitten(kittens, element) {
-  kittens.push(element);
+function destructivelyAppendKitten(kittens) {
+  kittens.push();
 }
 
-function destructivelyAppendKitten(kittens, element) {
-  kittens.unshift(element);
+function destructivelyAppendKitten(kittens) {
+  kittens.unshift();
 }
