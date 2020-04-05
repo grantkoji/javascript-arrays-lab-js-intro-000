6ccbe4a1be1faf6928a2f5d074ae2bbce59e5b1a
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-05 20:09:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function destructivelyAppendKitten(name) {
 function destructivelyPrependKitten(name) {
   kittens.unshift(name);
 }
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+}
+
